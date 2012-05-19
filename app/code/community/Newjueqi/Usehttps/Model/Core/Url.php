db796387a09ffdb63eb5bc05677b0a1c4c66a4db
zengjiansheng <h6k65@126.com>
2012-05-19 09:42:36 +0800

    let all urls in one page use the same protocol

--- app/code/community/Newjueqi/Usehttps/Model/Core/Url.php
+++ app/code/community/Newjueqi/Usehttps/Model/Core/Url.php
@@ -19,15 +19,22 @@ class Newjueqi_Usehttps_Model_Core_Url extends Mage_Core_Model_Url
     public function getUrl($routePath = null, $routeParams = null)
     {
 
-        //if it has set $routeParams,then add  '_secure' => true
-        if( is_array($routeParams) && !isset($routeParams['_secure']) ){
-        	$routeParams['_secure']=true;
-        }else{
-        	
-        	//set $routeParams to use https
-        	$routeParams=array('_secure'=>true);
-        }
-        
+        /*
+         use https
+         if url show in web browser start with https, all urls in the webpage must be start with https,
+         for example: if main page is https, but ajax request send in this page is http, then ajax request will fail
+                      if main page is http, but ajax request send in this page is https, then ajax request will fail also
+            
+        */
+        if( isset($_SERVER['SERVER_PORT']) && 443==$_SERVER['SERVER_PORT'] ){
+           //if it has set $routeParams,then add  '_secure' => true
+           if( is_array($routeParams) && !isset($routeParams['_secure']) ){
+             $routeParams['_secure']=true;
+           }else{      
+              //set $routeParams to use https
+              $routeParams=array('_secure'=>true);
+           }
+        }     
         return parent::getUrl($routePath,$routeParams);
       
     }
